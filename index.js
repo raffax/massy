778ab392c0df12a4d9744b87c87aac178180c989
raffax <38769277+raffax@users.noreply.github.com>
2018-04-28 14:19:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,28 +4,21 @@ http         = require('http'),
 server       = express()
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+const {WebhookClient} = require('dialogflow-fulfillment')
+
 server.get('/',function(req,res){
     res.send('We are happy to see you using Chat Bot Webhook');
     console.log("Accesso GET ");
 });
 server.post('/',function(req,res){   
     console.log("HOOK STARTED");   
-    if(req.body.result != null) {console.log(JSON.stringify(req.body.result));}
-    else {console.log("Post not containing result")}
-    if (req.body.result !=null && req.body.result.action != null && req.body.result.action == "chetempochefa") {
-        res.json({
-            speech: "Tempo bellissimo,sole a volontà",
-            displayText: "Tempo bellissimo,sole a volontà",
-            source: 'chetempochefa'
-        });
+    if(req.body.result == null) {
+        console.log("Error, request empty");
+        res.json("Errore xxx");
     }
-    else {
-        res.json({
-            speech: "Boh",
-            displayText: "Boh",
-            source: "boh"
-        });
-    }
+    console.log("request is ok");
+    const agent = new WebhookClient({ request, response });
+    agent.add("Bel tempo qui");
 })
 
 server.listen((process.env.PORT || 8000), function () {
